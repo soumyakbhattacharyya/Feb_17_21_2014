4a759e24e9422fd6bf98f2329339c30d38e10a80
Prabhu <iamprabhu@yahoo.com>
2014-02-17 14:16:53 +0530

    JS

---
+++ Day01/HelloWorld.js
@@ -0,0 +1,12 @@
+//This is JavaScript
+//Interpreted language
+//Case-sensitive
+//not an OO language
+//Functional prog. lang
+//loop, conditions similar to C (Java)
+//It can be used in the server side
+//Dynamically typed
+
+//console.Log("Test");
+console.log("WTH is going on???");
+console.log("Hello World");
