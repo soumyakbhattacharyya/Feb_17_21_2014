fc683a489f9313c1551aa0d348632754ecb87edf
Prabhu <iamprabhu@yahoo.com>
2014-02-17 14:35:11 +0530

    JS

---
+++ Day01/DataTypes.js
@@ -0,0 +1,22 @@
+var arr1 = [];
+var arr2 = ["Java","C#","Ruby"];
+console.log(arr2[0]);
+console.log(arr2[1]);
+console.log(arr2[10]);//undefined
+
+for(var i=0;i<arr2.length;i++){
+	console.log(arr2[i]);
+}
+
+
+
+
+var str1 = "JavaScript is cool";
+console.log(str1.length);
+console.log(str1.toUpperCase());
+console.log(str1.toLowerCase());
+
+var a = 100;
+var b = 200;
+var c = a+b;
+console.log("Sum of a and b is " + c);
