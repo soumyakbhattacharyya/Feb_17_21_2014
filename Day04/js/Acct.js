c1646536e7217af5c5d07966fae5027cb13045bf
Prabhu <iamprabhu@yahoo.com>
2014-02-21 16:30:14 +0530

    JS

--- Day04/js/Acct.js
+++ Day04/js/Acct.js
@@ -1,3 +1,16 @@
+function $(expr){
+	var id = expr.substring(1);
+	var element = document.getElementById(id);
+	return {
+		click : function(arg){
+			element.onclick = arg;
+		},
+		val : function(){
+			return element.value;
+		}
+	};
+}
+
 var Acct = {
 	main : function(jsonObj){
 		if(jsonObj.hasOwnProperty("launch"))
--- Day04/js/Day04.js
+++ Day04/js/Day04.js
@@ -4,6 +4,34 @@ Acct.main({
 		$("#addbutton").click(addButtonClicked);
 	}
 });
+
+function getDesignation(){
+	var designation = "";
+	if (document.getElementById('prog').checked) {
+  		designation = document.getElementById('prog').value;
+	}
+	if (document.getElementById('web').checked) {
+  		designation = document.getElementById('web').value;
+	}
+	if (document.getElementById('tester').checked) {
+  		designation = document.getElementById('tester').value;
+	}
+	return designation;
+}
+var infoArr = [];
 function addButtonClicked(){
-	alert("Add button clicked");
+	var infoObj = {};
+	infoObj["company"] = $("#company").val();
+	infoObj["location"] = $("#location").val();
+	infoObj["newsLetter"] = 
+		document.getElementById("newsletter").checked ? "Yes":"No";
+	infoObj["designation"] = getDesignation();
+	infoArr[infoArr.length] = infoObj;
+	console.log(infoArr);
+
+
+	//Fetch the values of all the form items
+	//Create a JSON object storing all the values
+	//Add the JSON object to an array
+	//alert("Add button clicked");
 }
