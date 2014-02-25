89dc842d620f32ccb9e08f743bf3369fd026285b
Prabhu <iamprabhu@yahoo.com>
2014-02-25 10:58:30 +0530

    JSON

--- Day06/ajaxEx1.js
+++ Day06/ajaxEx1.js
@@ -1,5 +1,39 @@
-window.onload = loadCountriesFromServer;
+window.onload = init;
 var xhr;
+var citiesArr = [];
+function init(){
+	loadCountriesFromServer();
+	document.getElementById("countries").onchange = loadCities;
+}
+function loadCities(){
+	var country = document.getElementById("countries").value;
+	if(citiesArr.hasOwnProperty(country)){
+		var cities = citiesArr[country];
+		populateCitiesListbox(cities);
+	}
+	else{
+		var url = "cities.jsp?country=" + country;
+		xhr.onreadystatechange = fetchCitiesFromServer;
+		xhr.open("GET",url);
+		xhr.send();		
+	}
+}
+function fetchCitiesFromServer(){
+	if(xhr.readyState == 4){
+		var jsonObj = JSON.parse(xhr.responseText);
+		var country = document.getElementById("countries").value;
+		citiesArr[country] = jsonObj.cities;
+		populateCitiesListbox(jsonObj.cities);
+	}
+}
+function populateCitiesListbox(cities){
+	var options = "<option>Select</option>";
+	for(var i=0;i<cities.length;i++){
+		var city = cities[i];
+		options += "<option>" + city + "</option>";
+	}
+	document.getElementById("cities").innerHTML = options;		
+}
 function loadCountriesFromServer(){
 	xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = populateCountriesListbox;
