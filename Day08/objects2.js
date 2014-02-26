c48ac7d056841bdb2cc132aa50c6532d77ecbbb6
Prabhu <iamprabhu@yahoo.com>
2014-02-26 10:30:34 +0530

    json

--- Day08/objects2.js
+++ Day08/objects2.js
@@ -1,3 +1,23 @@
+var p1 = new Person("Sam",12);
+p1.eat();
+console.log(p1.getName());
+
+function Person(theName,theAge){
+	var name = theName;
+	var age = theAge;
+	this.getName = function(){
+		return name;
+	};
+	this.getAge = function(){
+		return age;
+	};
+	this.eat = function(){
+		console.log(name + " is eating");
+	};
+	this.sleep = function(){
+		console.log(name + " is sleeping");
+	};
+}
 function Car(theModel,theYearOfMake){
 	this.model = theModel;
 	this.yearOfMake = theYearOfMake;
