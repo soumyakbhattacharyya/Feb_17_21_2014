bc7de66119adfde3e993f60d4d3655d73a66db89
Prabhu <iamprabhu@yahoo.com>
2014-03-13 12:24:55 +0530

    jQM underscore

---
+++ Day19/jqmunderscore1.js
@@ -0,0 +1,39 @@
+window.Lab = {};
+
+$().ready(function(){
+	Lab.guestTemplate = _.template($("#guesttemplate").html());
+	loadGuestBookFromServer();
+	$("#addguestbtn").on("click",addGuest);
+});
+function loadGuestBookFromServer(){
+	$.getJSON("guestbook.jsp",function(response){
+		var guestbook = response.guestbook;
+		for(var i=0;i<guestbook.length;i++){
+			var li = Lab.guestTemplate(guestbook[i]);
+			$("#guestbook").append(li);
+			$("#guestbook").listview("refresh");
+
+		}
+	});
+}
+function addGuest(){
+	var name = $("#name").val();
+	var address = $("#address").val();
+	var comments = $("#comments").val();	
+	$.ajax({
+		url : "guestbook.jsp",
+		dataType : "json",
+		data : {
+			action : "add",
+			name : name, address : address, comments : comments
+		},
+		success : function(response){
+			var li = Lab.guestTemplate(response);
+			$("#guestbook").append(li);
+			$("#guestbook").listview("refresh");
+			$("#addguestpopup").popup("close");
+			$("#name,#address,#comments").val("");
+		}
+	});
+	
+}
