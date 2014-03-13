0dc5f0d1dbc1056862177af5718d76416a26a56a
Prabhu <iamprabhu@yahoo.com>
2014-03-13 12:38:36 +0530

    underscore

--- Day19/jqmunderscore1.js
+++ Day19/jqmunderscore1.js
@@ -2,18 +2,24 @@ window.Lab = {};
 
 $().ready(function(){
 	Lab.guestTemplate = _.template($("#guesttemplate").html());
+	Lab.guestBookTemplate = _.template($("#guestbooktemplate").html());
 	loadGuestBookFromServer();
 	$("#addguestbtn").on("click",addGuest);
 });
 function loadGuestBookFromServer(){
 	$.getJSON("guestbook.jsp",function(response){
 		var guestbook = response.guestbook;
-		for(var i=0;i<guestbook.length;i++){
+		var output = Lab.guestBookTemplate(response);
+		$("#guestbook").append(output);
+		$("#guestbook").listview("refresh");
+		
+/*		for(var i=0;i<guestbook.length;i++){
 			var li = Lab.guestTemplate(guestbook[i]);
 			$("#guestbook").append(li);
 			$("#guestbook").listview("refresh");
 
 		}
+*/		
 	});
 }
 function addGuest(){
